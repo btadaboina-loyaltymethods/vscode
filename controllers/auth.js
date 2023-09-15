f972ea96393cf5956af5115bb553efa888558f89
Btadaboina-loyaltymethods <Btadaboina@loyaltymethods.com>
2023-09-15 10:54:19 +0530

    validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
-// const sendgridTransport = require("nodemailer-sendgrid-transport");
-// const nodemailer = require("nodemailer");
+const {validationResult} = require("express-validator");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -82,6 +81,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email }).then((userDoc) => {
     if (userDoc) {
       req.flash("error", "Already existing user? Try login.");
@@ -287,9 +294,7 @@ exports.postNewPassword = (req, res, next) => {
       return res.redirect("/login");
     })
     .catch((err) => {
-      console.log(
-        err
-      );
+      console.log(err);
       // Handle other errors
       req.flash("error", "An error occurred while resetting the password.");
       return res.redirect("/reset");
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,28 @@
 const express = require("express");
 // const { csrfProtection } = require("../app");
+const { check, body } = require("express-validator");
+
+// console.log(ep.check, "k8tgyhunjmiljgds");
+
 const authController = require("../controllers/auth");
+// const { check } = require("express-validator");
 
 const router = express.Router();
-
 router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email").isEmail(),
+    // console.log(ev.body()),
+    body("password", "Please enter a valid email or password")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postSignup
+);
 
 router.post("/login", authController.postLogin);
 
